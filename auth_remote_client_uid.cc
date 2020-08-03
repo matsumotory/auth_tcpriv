bfac0aa98d621cc1b5e8952e875dd79d829507df
matsumotory <matsu1229@gmail.com>
2020-08-03 17:51:55 +0900

    First commit

---
+++ auth_remote_client_uid.cc
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  auth_remote_client_uid.cc
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  08/03/2020 17:51:10
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+
