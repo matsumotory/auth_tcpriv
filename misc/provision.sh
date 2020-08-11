92369956d7abacc0b0f2ccd5b05565400ed3744a
matsumotory <matsu1229@gmail.com>
2020-08-11 19:22:55 +0900

    Downloading mysql-server source

--- misc/provision.sh
+++ misc/provision.sh
@@ -1,9 +1,10 @@
 #/bin/bash
 
 MYHOST=`hostname`
-SRC_DIR=~/tcp_save_syn_listen
+SRC_DIR=~/auth_remote_client_uid
 BUILD_DIR=$SRC_DIR/build
 TEST_DIR=$SRC_DIR/test
+REPO=https://github.com/matsumotory/auth_remote_client_uid.git
 
 # use ccache
 HOSTCXX=g++
@@ -14,7 +15,7 @@ THREAD=2
 if [ -d $SRC_DIR ]; then
   rm -rf $SRC_DIR
 fi
-git clone https://github.com/matsumotory/auth_remote_client_uid.git $SRC_DIR
+git clone $REPO $SRC_DIR
 
 # setup build enviroment
 sudo apt-get update
@@ -25,6 +26,9 @@ sudo apt-get -y remove nano
 sudo apt-get -y install gawk chrpath socat libsdl1.2-dev xterm libncurses5-dev lzop flex libelf-dev kmod
 
 sudo update-alternatives --install /usr/bin/clang-format clang-format /usr/bin/clang-format-6.0 1000
+sudo sed -i s/#deb-src/deb-src/ /etc/apt/sources.list
+sudo apt update
+sudo apt install -y dpkg-dev
 
 if [ -d $BUILD_DIR ]; then
   rm -rf $BUILD_DIR
@@ -32,6 +36,8 @@ fi
 mkdir $BUILD_DIR
 
 if [ $MYHOST = "server" ]; then
+  cd $SRC_DIR
+  apt source mysql-server
   cd $TEST_DIR
   make clean
   make
